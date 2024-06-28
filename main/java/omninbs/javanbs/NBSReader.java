61b1637f1a16812fa07345dbe5c05cd48b33b54e
John-Bush14 <joppe@stuyck.be>
2024-06-28 23:36:51 +0200

    setup: nbsReader/Writer

---
+++ main/java/omninbs/javanbs/NBSReader.java
@@ -0,0 +1,13 @@
+public class NBSReader {
+   public int readBytes(DataInputStream dis, int bytes) throws IOException {
+      
+   }
+
+   public String readString(DataInputStream dis) throws IOException {
+      
+   }
+
+   public NBSSong readSong(DataInputStream dis) throws IOException {
+      
+   }
+}
---
+++ main/java/omninbs/javanbs/NBSWriter.java
@@ -0,0 +1,21 @@
+public enum Binary {
+    Byte,
+    UByte,
+    Short,
+    Integer,
+    String
+}
+
+public class NBSWriter {
+   public void writeBytes(DataInputStream dis, int data, Binary type) throws IOException {
+      
+   }
+
+   public void writeString(DataInputStream dis, String string) throws IOException {
+      
+   }
+
+   public void writeSong(DataInputStream dis, NBSong song, int version) throws IOException {
+      
+   }
+}
