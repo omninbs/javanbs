b9d68c96bb70fa78b866dd06bedd00d848dda4af
John-Bush14 <joppe@stuyck.be>
2024-06-28 23:49:13 +0200

    feat: get/setters NBSLayer

--- main/java/omninbs/javanbs/model/NBSLayer.java
+++ main/java/omninbs/javanbs/model/NBSLayer.java
@@ -1,3 +1,26 @@
 public class NBSLayer {
+   private String name;
+   private bool lock;
+   private int volume;
+   private int stereo;
 
+
+   // setters
+   public void setName(String newName) {this.name = newName}
+
+   public void setLock(bool newLock) {this.lock = lock}
+
+   public void setVolume(int newVolume) {this.volume = newVolume}
+
+   public void setStereo(int newStereo) {this.stereo = newStereo}
+
+
+   // getters
+   public String getName() {return name}
+
+   public String getLock() {return lock}
+
+   public String getVolume() {return volume}
+
+   public String getStereo() {return stereo}
 }
