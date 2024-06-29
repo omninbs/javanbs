599f19bb46d6eb04d3ea9562164b207601ed63c4
John-Bush14 <joppe@stuyck.be>
2024-06-29 16:44:49 +0200

    feat: NBSNote set limits

--- src/main/java/omninbs/javanbs/model/NBSNote.java
+++ src/main/java/omninbs/javanbs/model/NBSNote.java
@@ -6,14 +6,16 @@ public class NBSNote {
    private int pitch;
    private int instrument;
    private int key;
+   private int stereo;
    private int layer;
    private int tick;
 
-   public NBSNote(int instrument, int key, int pitch, int volume) {
+   public NBSNote(int instrument, int key, int pitch, int volume, int stereo) {
       setInstrument(instrument);
       setKey(key);
       setPitch(pitch);
       setVolume(volume);
+      setStereo(stereo);
 
       setLayer(-1);
       setTick(-1);
@@ -29,17 +31,40 @@ public class NBSNote {
    
 
    // setters
-   public void setVolume(int newVolume) {this.volume = newVolume}
+   public void setVolume(int newVolume) {
+      if (0 > newVolume || newVolume > 100) {throw new IllegalArgumentException("volume needs to be number between 0 and 100");}
+      this.volume = newVolume;
+   }
 
-   public void setPitch(int newPitch) {this.pitch = newPitch}
+   public void setPitch(int newPitch) {
+      if (-32768 > newPitch || newpitch > 32,768) {throw new IllegalArgumentException("pitch needs to be a number between -32,768 and 32,768");}
+      this.pitch = newPitch
+   }
 
-   public void setInstrument(int newInstrument) {this.instrument = newInstrument}
+   public void setInstrument(int newInstrument) {
+      if (0 > newInstrument || newInstrument > 256) {throw new IllegalArgumentException("instrument needs to be number between 0 and 256");}
+      this.instrument = newInstrument;
+   }
 
-   public void setKey(int newKey) {this.key = newKey}
+   public void setKey(int newKey) {
+      if (0 > newKey || newKey > 83) {throw new IllegalArgumentException("key needs to be a number between 0 and 83");}
+      this.key = newKey;
+   }
 
-   public void setLayer(int newLayer) {this.layer = newLayer}
+   public void setLayer(int newLayer) {
+      if (-1 > newLayer) {throw new IllegalArgumentException("key needs to be a number above -1");}
+      this.layer = newLayer;
+   }
 
-   public void setTick(int newTick) {this.tick = newTick}
+   public void setTick(int newTick) {
+      if (-1 > newTick) {throw new IllegalArgumentException("tick needs to be a number above -1");}
+      this.tick = newTick;
+   }
+   
+   public void setStereo(int newStereo) {
+      if (-100 > newStereo || newStereo >  100) {throw new IllegalArgumentException("stereo needs to be a number between -100 and 100");}
+      this.stereo = newStereo;
+   }
 
 
    // getters
@@ -54,5 +79,7 @@ public class NBSNote {
    public int getLayer() {return layer;}
 
    public int getTick() {return tick;}
+
+   public int getStereo() {return stereo;}
 
 }
