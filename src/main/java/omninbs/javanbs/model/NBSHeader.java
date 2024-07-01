4ea5339aa845270e5e92e2c9d12d66cf31614f75
John-Bush14 <joppe@stuyck.be>
2024-07-01 18:49:10 +0200

    fix(header): correct types + reading

--- src/main/java/omninbs/javanbs/model/NBSHeader.java
+++ src/main/java/omninbs/javanbs/model/NBSHeader.java
@@ -1,9 +1,13 @@
 package omninbs.javanbs.model;
 
+import omninbs.javanbs.NBSReader;
+import omninbs.javanbs.NBSWriter;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class NBSHeader {
@@ -16,7 +20,7 @@ public class NBSHeader {
    private String originalAuthor;
    private String description;
    private int tempo;
-   private int autoSaving;
+   private boolean autoSaving;
    private int autoSavingDuration;
    private int timeSignature;
    private int minutesSpent;
@@ -25,41 +29,103 @@ public class NBSHeader {
    private int noteblocksAdded;
    private int noteblocksRemoved;
    private String songOrigin;
-   private int looping;
+   private boolean looping;
    private int maxLoops;
    private int loopStart;
-   
-   public static List<NBSNote> readNotes(DataInputStream dis) throws IOException {
-      throw new IllegalArgumentException("TODO!");
-   }
-   
-   public void writeNotes(DataInputStream dis) throws IOException {
-      
+
+   public static NBSHeader readHeader(FileInputStream fis) throws IOException {
+      int version = 0;
+      NBSHeader header = new NBSHeader("");
+
+      header.length = NBSReader.readBytes(fis, 2);
+
+      if (header.length == 0) { // version > 0
+         header.version = NBSReader.readBytes(fis, 1);
+         header.defaultInstruments = NBSReader.readBytes(fis, 1);
+         if (header.version >= 3) {header.length = NBSReader.readBytes(fis, 2);}
+      }
+
+      header.layerCount = NBSReader.readBytes(fis, 2);
+      header.name = NBSReader.readString(fis);
+      header.author = NBSReader.readString(fis);
+      header.originalAuthor = NBSReader.readString(fis);
+      header.description = NBSReader.readString(fis);
+      header.tempo = NBSReader.readBytes(fis, 2);
+      header.autoSaving = NBSReader.readBytes(fis, 1) != 0;
+      header.autoSavingDuration = NBSReader.readBytes(fis, 1);
+      header.timeSignature = NBSReader.readBytes(fis, 1);
+      header.minutesSpent = NBSReader.readBytes(fis, 4);
+      header.leftClicks = NBSReader.readBytes(fis, 4);
+      header.rightClicks = NBSReader.readBytes(fis, 4);
+      header.noteblocksAdded = NBSReader.readBytes(fis, 4);
+      header.noteblocksRemoved = NBSReader.readBytes(fis, 4);
+      header.songOrigin = NBSReader.readString(fis);
+
+      if (header.version >= 4) {
+         header.looping = NBSReader.readBytes(fis, 1) != 0;
+         header.maxLoops = NBSReader.readBytes(fis, 1);
+         header.loopStart = NBSReader.readBytes(fis, 2);
+      }
+
+      return header;
    }
 
-       // Constructor
-    public NBSHeader(String name) {
-        this.name = name;
-        this.version = 5;
-        this.defaultInstruments = 15;
-        this.length = 0;
-        this.layerCount = 1;
-        this.author = "";
-        this.originalAuthor = "";
-        this.description = "";
-        this.tempo = 20;
-        this.autoSaving = 0;
-        this.autoSavingDuration = 0;
-        this.timeSignature = 0;
-        this.minutesSpent = 0;
-        this.leftClicks = 0;
-        this.rightClicks = 0;
-        this.noteblocksAdded = 0;
-        this.noteblocksRemoved = 0;
-        this.songOrigin = "";
-        this.looping = 0;
-        this.maxLoops = 0;
-        this.loopStart = 0;
+   public void writeHeader(FileOutputStream fos) throws IOException {
+      if (version == 0) {NBSWriter.writeBytes(fos, length, 2);}
+      else {
+         NBSWriter.writeBytes(fos, 0, 2);
+         NBSWriter.writeBytes(fos, version, 1);
+         NBSWriter.writeBytes(fos, defaultInstruments, 1);
+         if (version >= 3) {NBSWriter.writeBytes(fos, length, 2);}
+      }
+      
+      NBSWriter.writeBytes(fos, layerCount, 2);
+      NBSWriter.writeString(fos, name);
+      NBSWriter.writeString(fos, author);
+      NBSWriter.writeString(fos, originalAuthor);
+      NBSWriter.writeString(fos, description);
+      NBSWriter.writeBytes(fos, tempo, 2);
+      NBSWriter.writeBytes(fos, autoSaving ? 1 : 0, 1);
+      NBSWriter.writeBytes(fos, autoSavingDuration, 1);
+      NBSWriter.writeBytes(fos, timeSignature, 1);
+      NBSWriter.writeBytes(fos, minutesSpent, 4);
+      NBSWriter.writeBytes(fos, leftClicks, 4);
+      NBSWriter.writeBytes(fos, rightClicks, 4);
+      NBSWriter.writeBytes(fos, noteblocksAdded, 4);
+      NBSWriter.writeBytes(fos, noteblocksRemoved, 4);
+      NBSWriter.writeString(fos, songOrigin);
+
+      if (version >= 4) {
+         NBSWriter.writeBytes(fos, looping ? 1 : 0, 1);
+         NBSWriter.writeBytes(fos, maxLoops, 1);
+         NBSWriter.writeBytes(fos, loopStart, 2);
+      }
+   }   
+
+
+   // Constructor
+   public NBSHeader(String name) {
+      this.name = name;
+      this.version = 5;
+      this.defaultInstruments = 15;
+      this.length = 0;
+      this.layerCount = 1;
+      this.author = "";
+      this.originalAuthor = "";
+      this.description = "";
+      this.tempo = 20;
+      this.autoSaving = true;
+      this.autoSavingDuration = 0;
+      this.timeSignature = 0;
+      this.minutesSpent = 0;
+      this.leftClicks = 0;
+      this.rightClicks = 0;
+      this.noteblocksAdded = 0;
+      this.noteblocksRemoved = 0;
+      this.songOrigin = "";
+      this.looping = false;
+      this.maxLoops = 0;
+      this.loopStart = 0;
     }
 
 
@@ -100,7 +166,7 @@ public class NBSHeader {
         this.tempo = tempo;
     }
 
-    public void setAutoSaving(int autoSaving) {
+    public void setAutoSaving(boolean autoSaving) {
         this.autoSaving = autoSaving;
     }
 
@@ -136,7 +202,7 @@ public class NBSHeader {
         this.songOrigin = songOrigin;
     }
 
-    public void setLooping(int looping) {
+    public void setLooping(boolean looping) {
         this.looping = looping;
     }
 
@@ -185,7 +251,7 @@ public class NBSHeader {
         return tempo;
     }
 
-    public int getAutoSaving() {
+    public boolean getAutoSaving() {
         return autoSaving;
     }
 
@@ -221,7 +287,7 @@ public class NBSHeader {
         return songOrigin;
     }
 
-    public int getLooping() {
+    public boolean getLooping() {
         return looping;
     }
 
