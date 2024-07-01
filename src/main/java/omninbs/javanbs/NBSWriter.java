a4c5f8d2448ecbb10230d57fdfe80b073a48a191
John-Bush14 <joppe@stuyck.be>
2024-07-01 18:49:50 +0200

    feat: byte/string writing

--- src/main/java/omninbs/javanbs/NBSWriter.java
+++ src/main/java/omninbs/javanbs/NBSWriter.java
@@ -1,20 +1,38 @@
 package omninbs.javanbs;
 
-import java.io.DataInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import omninbs.javanbs.model.NBSSong;
 
 public class NBSWriter {
-   public static void writeBytes(DataInputStream dis, int data, int type) throws IOException {
-      
+   public static void writeBytes(FileOutputStream fos, int value, int bytes, boolean signed) throws IOException {
+      if (bytes == 1) {
+         if (signed) {
+            fos.write((byte) value);
+         } else {
+            fos.write(value & 0xFF);
+         }
+      } else {
+         for (int i = bytes - 1; i >= 0; i--) {
+            fos.write((value >> (8 * i)) & 0xFF);
+         }
+      }
+   }
+   
+   public static void writeBytes(FileOutputStream fos, int data, int bytes) throws IOException {writeBytes(fos, data, bytes, true);}
+
+   public static void writeString(FileOutputStream fos, String string) throws IOException {
+      writeBytes(fos, string.length(), 4);
+
+      for (int i = 0; i < string.length(); i++) {
+         char c = string.charAt(i);
+
+         fos.write((byte) c);
+      }
    }
 
-   public static void writeString(DataInputStream dis, String string) throws IOException {
-      
-   }
-
-   public static void writeSong(DataInputStream dis, NBSSong song, int version) throws IOException {
+   public static void writeSong(FileOutputStream fos, NBSSong song, int version) throws IOException {
       
    }
 }
