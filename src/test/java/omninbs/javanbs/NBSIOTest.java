c3e62885229abbbb6ca6e5fd06554557eed98edb
John-Bush14 <joppe@stuyck.be>
2024-07-01 19:52:01 +0200

    feat: Song IO testing

---
+++ src/test/java/omninbs/javanbs/NBSIOTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import omninbs.javanbs.NBSReader;
+import omninbs.javanbs.NBSWriter;
+import omninbs.javanbs.model.NBSSong;
+
+public class NBSIOTest {
+   
+   @Test
+   public void testSongIO() throws IOException {
+      String testFile = "Megalovania.nbs";
+      String output = "out.out";
+
+      testFile = getClass().getClassLoader().getResource(testFile).getFile();
+      output = Paths.get("src", "test", "resources", output).toString();
+      
+      System.out.println("SongIO test start!");
+
+      NBSSong song = NBSReader.readSong(testFile);
+
+      System.out.println("OG Reading done!");
+
+      NBSWriter.writeSong(output, song);
+      
+      System.out.println("OG Writing done!");
+
+      assertEquals(NBSReader.readSong(output), song);
+      
+      System.out.println("SH Reading done!");
+   }
+}
